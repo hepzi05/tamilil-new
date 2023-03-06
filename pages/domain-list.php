e6be4076a1f307563387bde3733fdd09cbd3755b
veroah <hepzi4595@gmail.com>
2023-03-06 23:56:24 +0530

    domain_list

--- pages/domain-list.php
+++ pages/domain-list.php
@@ -4,20 +4,19 @@ require "sidebar.php";
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-5">
     <input type="text" placeholder="Search for your next domain" class="w-100 mt-5 py-3 px-3 border border-0 shadow-lg rounded-3" />
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom mt-4">
+    <div class="pt-3 pb-2 mb-3 border-bottom mt-4">
         <div>
             <h3 class="text-secondary">Domain List</h3>
             <br><br>
-            <table class="table table-stripped table-hover">
+            <table class="table" id="table_id">
                 <thead>
                     <tr>
-                        <td>ID</td>
-                        <td>Name</td>
-                        <td>Created</td>
-                        <td>Expires</td>
-                        <td>Is locaked</td>
-                        <td>Auto renew</td>
-                        <td>Premium</td>
+                        <th>S.No</th>
+                        <th>Name</th>
+                        <th>Created</th>
+                        <th>Is locked</th>
+                        <th>Auto renew</th>
+                        <th>Premium</th>
                     </tr>
                 </thead>
                 <tbody id="tbody">
@@ -28,7 +27,12 @@ require "sidebar.php";
     </div>
 </main>
 
+<script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+<link href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css" rel="stylesheet" />
 <script>
+    $(document).ready(function() {
+    $('#table_id').DataTable();
+});
     // function checkLogin() {
     const username = "siddhamedicine";
 
@@ -48,10 +52,10 @@ require "sidebar.php";
                 // window.location.replace("dashboard.php");
 
                 var table = document.getElementById("tbody");
-
-                response.map(item => {
+                table.innerHTML = "";
+                response.map((item,index) => {
                     console.log(item);
-                    table.innerHTML += " <tr> <td>" + item.attributes.ID + "</td>" + item.attributes.NAME + "<td></td> <td>" + item.attributes.CREATED + "</td> <td>" + item.attributes.ISEXPIRED + "</td> <td>" + item.attributes.ISLOCKED + "</td> <td>" + item.attributes.ISPREMIUM + "</td> <td>" + item.attributes.ISPREMIUM + "</td> </tr> ";
+                    table.innerHTML += " <tr> <td>" + (index+1)  + "</td> <td>" + item.attributes.NAME + "</td> <td>" + item.attributes.CREATED + "</td> <td class="+(item.attributes.ISLOCKED === "true"?"text-success":"text-danger")+">" + item.attributes.ISLOCKED + "</td> <td>" + (item.attributes.AUTORENEW === "true" ? "on" : "off") + "</td> <td>" +( item.attributes.ISPREMIUM === "true"?"yes":"no") + "</td> </tr> ";
                 })
 
             } else {
