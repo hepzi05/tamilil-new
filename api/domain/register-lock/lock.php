98941138988a733afd27f8d39b19ffeb02bb516c
bharathi <bharathikannanr1999@gmail.com>
2023-03-07 20:43:18 +0530

    Structured api folders, locks

--- api/domain/register-lock/lock.php
+++ api/domain/register-lock/lock.php
@@ -1,18 +1,19 @@
 <?php
-require("../variables.php");
+require("../../variables.php");
 $xmlparser = xml_parser_create();
 session_start();
 
 $username = $_POST['username'];
+$domainname = $_POST['domainname'];
+$lockaction = $_POST['lockaction'];
 
 if (!defined('NAMECHEAP_DOMAIN') && !defined('NAMECHEAP_APIUSER') && !defined('NAMECHEAP_APIKEY') && !defined('NAMECHEAP_CLIENTIP')) {
     echo "Config variables are missing...";
 }
 
 try {
-
     $response = file_get_contents(
-        NAMECHEAP_DOMAIN . "?ApiUser=" . NAMECHEAP_APIUSER . "&ApiKey=" . NAMECHEAP_APIKEY . "&UserName=$username&ClientIp=" . NAMECHEAP_CLIENTIP . "&Command=namecheap.ssl.getList"
+        NAMECHEAP_DOMAIN . "?ApiUser=" . NAMECHEAP_APIUSER . "&ApiKey=" . NAMECHEAP_APIKEY . "&UserName=$username&ClientIp=" . NAMECHEAP_CLIENTIP . "&Command=namecheap.domains.setRegistrarLock&DomainName=$domainname&LockAction=$lockaction"
     );
 
     xml_parse_into_struct($xmlparser, $response, $values);
@@ -26,7 +27,7 @@ try {
         $filteredArr = [];
 
         foreach ($values as $item) {
-            if ($item["tag"] == "SSL") {
+            if ($item["tag"] == "DOMAINSETREGISTRARLOCKRESULT") {
                 array_push($filteredArr, $item);
             }
         }
@@ -35,4 +36,5 @@ try {
     }
 } catch (Exception $e) {
     echo $e;
+    print_r($e);
 }
