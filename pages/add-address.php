f63e097ed2dd174ef1dbd23cf5e177b5e960341f
veroah <hepzi4595@gmail.com>
2023-03-13 00:12:16 +0530

    add-address,dashboard

--- pages/add-address.php
+++ pages/add-address.php
@@ -9,39 +9,69 @@ require "sidebar.php";
             <h4 class="text-secondary">Add Primary Address</h4>
         </div>
     </div>
-    <h4 class="text-secondary mt-4">Basic Information</h4>
-
-    <h4 class="text-secondary mt-4">Street Information</h4>
     <div class="row">
         <div class="col-ld-9 col-md-9 col-sm-12">
+            <h4 class="text-secondary mt-4">Basic Information</h4>
             <div class="form-group d-flex flex-row justify-content-between mt-4">
-                <label class="fw-bold text-secondary">Address*</label>
+                <label class="fw-bold text-secondary mt-2">Address Name</label>
+                <input type="text" class="form-control w-75" id="firstname" value="Primary Address" disabled />
+            </div>
+            <div class="form-group d-flex flex-row justify-content-between mt-4">
+                <label class="fw-bold text-secondary mt-2">First Name*</label>
+                <input type="text" class="form-control w-75" id="firstname" placeholder="First Name" />
+            </div>
+            <div class="form-group d-flex flex-row justify-content-between mt-4">
+                <label class="fw-bold text-secondary mt-2">Last Name*</label>
+                <input type="text" class="form-control w-75" id="lastname" placeholder="Last Name" />
+            </div>
+
+            <h4 class="text-secondary mt-4">Street Information</h4>
+            <div class="form-group d-flex flex-row justify-content-between mt-4">
+                <label class="fw-bold text-secondary mt-2">Address*</label>
                 <input type="text" class="form-control w-75" id="add1" placeholder="Address Line 1" />
             </div>
             <div class="mt-3">
-                <input type="text" class="form-control w-75" id="add2" placeholder="Address Line 2" />
+                <input type="text" class="form-control w-75 float-end" id="add2" placeholder="Address Line 2" />
             </div>
+            <div class="clearfix"></div>
             <div class="form-group d-flex flex-row justify-content-between mt-4">
-                <label class="fw-bold text-secondary">State/Province</label>
+                <label class="fw-bold text-secondary mt-2">State/Province</label>
                 <input type="text" class="form-control w-75" id="state" placeholder="State/Province" />
             </div>
             <div class="form-group d-flex flex-row justify-content-between mt-4">
-                <label class="fw-bold text-secondary">City*</label>
+                <label class="fw-bold text-secondary mt-2">City*</label>
                 <input type="text" class="form-control w-75" id="city" placeholder="City" />
             </div>
             <div class="form-group d-flex flex-row justify-content-between mt-4">
-                <label class="fw-bold text-secondary">ZIP/Postal Code</label>
-                <input type="text" class="form-control w-75" id="zip" placeholder="ZIP/Postal Code" />
+                <label class="fw-bold text-secondary mt-2">ZIP/Postal Code</label>
+                <input type="number" class="form-control w-75" id="zip" placeholder="ZIP/Postal Code" />
             </div>
             <div class="form-group d-flex flex-row justify-content-between mt-4">
-                <label class="fw-bold text-secondary">Country*</label>
+                <label class="fw-bold text-secondary mt-2">Country*</label>
                 <input type="text" class="form-control w-75" id="country" placeholder="Country" />
+            </div>
+
+            <h4 class="text-secondary mt-4">Contact Information</h4>
+
+            <div class="form-group d-flex flex-row justify-content-between mt-4">
+                <label class="fw-bold text-secondary mt-2">Phone Numer*</label>
+                <input type="number" class="form-control w-75" id="phone" placeholder="Phone Numer" />
+            </div>
+            <div class="fom-group d-flex flex-row justify-content-between mt-4">
+                <label class="fw-bold text-secondary mt-2">Fax Number</label>
+                <input type="number" class="form-control w-75" id="fax" placeholder="Fax Number" />
+            </div>
+            <div class="form-group d-flex flex-row justify-content-between mt-4">
+                <label class="fw-bold text-secondary mt-2">Email Address</label>
+                <input type="email" value="bharathi@gmail.com" class="form-control w-75" id="email" disabled />
+            </div>
+            <div class="mt-5 mb-5 text-center">
+                <button id="save" class="btn btn-primary">Save Changes</button>
+                <button id="cancel" class="btn btn-danger ms-3">Cancel</button>
             </div>
         </div>
         <div class="col-ld-3 col-md-3 col-sm-12"></div>
         <div class="row"></div>
     </div>
-
-    <h4 class="text-secondary mt-4">Contact Information</h4>
 
 </main>
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,16 +2,17 @@
 require "sidebar.php";
 ?>
 
+<?php $user=$_SESSION['user-details'];?>
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-5">
     <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <div>
-            <h4 class="text-secondary" id="hello">Hello</h4>
+            <h4 class="text-secondary">Hello <?php echo $user['FIRSTNAME']?></h4>
             <span>Last logged in on Feb 19, 2023, 07:18 AM (EST)</span>
         </div>
         <div>
             <span class="fw-bold">Account Balance</span>
-            <p>$0.00</p>
+            <p id="balance"></p>
         </div>
     </div>
     <h5 class="text-center mt-5"><b>Find your dream domain.</b>Your digital success story starts here.</h5
@@ -22,7 +23,20 @@ require "sidebar.php";
     </div>
 </main>
 <script>
-    var user = json.parse(sessionStorage.getItem("user"));
-    
-    document.getElementById("hello").user.FIRSTNAME;
+const username = "<?php echo $user['USERNAME']?>"
+
+$.post({
+    url: "../api/users/account-balance/balance.php",
+    data: {
+        username: username,
+    },
+    success: function(result) {
+        const response = JSON.parse(result);
+        document.getElementById("balance").innerHTML = "$ " + response['ACCOUNTBALANCE'];
+    },
+    error: function(error) {
+        console.log(JSON.stringify(error), "error");
+    }
+
+});
 </script>
